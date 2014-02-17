034637e35adc2771e026f7f3531194c96c08e964
Dan Johnson <djboy71@gmail.com>
2014-02-17 15:50:26 -0600

    Completed recipe 6.7

--- src/main/java/com/springinpractice/ch06/dao/hbn/HbnAccountDao.java
+++ src/main/java/com/springinpractice/ch06/dao/hbn/HbnAccountDao.java
@@ -12,10 +12,13 @@ import javax.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.authentication.dao.SaltSource;
+import org.springframework.security.authentication.encoding.PasswordEncoder;
 import org.springframework.stereotype.Repository;
 
 import com.springinpractice.ch06.dao.AccountDao;
 import com.springinpractice.ch06.domain.Account;
+import com.springinpractice.ch06.domain.UserDetailsAdapter;
 import com.springinpractice.dao.hibernate.AbstractHbnDao;
 
 /**
@@ -23,19 +26,27 @@ import com.springinpractice.dao.hibernate.AbstractHbnDao;
  */
 @Repository
 public class HbnAccountDao extends AbstractHbnDao<Account> implements AccountDao {
+	
 	private static final Logger log = LoggerFactory.getLogger(HbnAccountDao.class);
 
 	private static final String UPDATE_PASSWORD_SQL =
 		"update account set password = ? where username = ?";
 
 	@Inject private JdbcTemplate jdbcTemplate;
+	@Inject private PasswordEncoder passwordEncoder;
+	@Inject private SaltSource saltSource;
 
 	public void create(Account account, String password) {
 		log.debug("Creating Account");
 		create(account);
+		
+		log.debug("Encoding password");
+		Object salt = saltSource.getSalt(new UserDetailsAdapter(account));
+		String encPassword =
+				passwordEncoder.encodePassword(password, salt);
 
 		log.debug("Updating password");
-		jdbcTemplate.update(UPDATE_PASSWORD_SQL, password, account.getUsername());
+		jdbcTemplate.update(UPDATE_PASSWORD_SQL, encPassword, account.getUsername());
 	}
 
 	public Account findByUsername(String username) {
